ad1bad6f0c7755a0abae46eaf8f93f8fcbf60362
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-09-30 12:03:29 +0100

    linter added

--- app.rb
+++ app.rb
@@ -1,24 +1,24 @@
 class MorseCode
   MORSE_CODE = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-    "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-    "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-    ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-    "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-    "--.." => "Z", "-----" => "0", ".----" => "1", "..---" => "2", "...--" => "3",
-    "....-" => "4", "....." => "5", "-...." => "6", "--..." => "7", "---.." => "8",
-    "----." => "9"
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z', '-----' => '0', '.----' => '1', '..---' => '2', '...--' => '3',
+    '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8',
+    '----.' => '9'
   }
   def decode_char(morse_code)
     MORSE_CODE[morse_code]
   end
 
   def decode_word(morse_word)
-    return morse_word.split(" ").map { |char| decode_char(char) }.join
+    return morse_word.split(' ').map { |char| decode_char(char) }.join
   end
 
   def decode_msg(morse_msg)
-    return morse_msg.split("   ").map { |word| decode_char(word) }.join(" ")
+    return morse_msg.split('   ').map { |word| decode_char(word) }.join(' ')
   end
 end
 
@@ -26,6 +26,6 @@ end
 morse_code_decoder = MorseCode.new()
 
 # Decode the message from the old bottle
-message_in_bottle = "-- -.--   -. .- -- ."
+message_in_bottle = '-- -.--   -. .- -- .'
 decoded_message = morse_code_decoder.decode_msg(message_in_bottle)
 puts decoded_message
